c0c0ff3206e8dc2a5ae8204f4ed6d0ee7ed56f2a
JJ Merelo <jjmerelo@gmail.com>
2017-09-17 20:16:54 +0200

    Another test

--- HitosIV_test.go
+++ HitosIV_test.go
@@ -2,11 +2,20 @@ package HitosIV
 
 import (
 	"testing"
+	"reflect"
 )
 
 func TestHitos (t *testing.T){
 	t.Log("Test Id");
 	if CuantosHitos() <= 0 {
 		t.Error("No milestones")
+	}
+}
+
+func TestTodosHitos (t *testing.T){
+	t.Log("Test Todos");
+	these_milestones := Hitos()
+	if reflect.TypeOf(these_milestones).String() == "Data" {
+		t.Error("No milestones here")
 	}
 }
