e41132d920d3780c64984be38d4e4b8b0a879085
Alberto Marley <berbus8@gmail.com>
2017-09-29 19:06:50 +0200

    asd

--- HitosIV_test.go
+++ HitosIV_test.go
@@ -24,7 +24,8 @@ func TestUno(t *testing.T){
 	t.Log("Test Uno");
 	var x int = int(CuantosHitos());
 	for i:=0; i < x; i++{
-		if reflect.TypeOf(Uno(i)).String() == "HitosIV.Hito"{
+		var aux uint = uint(i);
+		if reflect.TypeOf(Uno(aux)).String() == "HitosIV.Hito"{
 			t.Log("El hito %d ha sido comprobado.", i);
 		}else{
 			t.Error("El hito %d no es del tipo correcto", i);
