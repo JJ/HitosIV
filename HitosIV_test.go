ca54377d7eb68e54ea6bab7848ad0e7b9dc18a71
Alberto Marley <berbus8@gmail.com>
2017-09-29 19:13:37 +0200

    Commit final. Deshago cambios en travis.yml. Quito comentarios inútiles.

--- HitosIV_test.go
+++ HitosIV_test.go
@@ -26,36 +26,9 @@ func TestUno(t *testing.T){
 	for i:=0; i < x; i++{
 		var aux uint = uint(i);
 		if reflect.TypeOf(Uno(aux)).String() == "HitosIV.Hito"{
-			t.Log("El hito ", i, "ha sido comprobado.");
+			t.Log("El hito", i, "ha sido comprobado.");
 		}else{
-			t.Error("El hito", i, " no es del tipo correcto");
+			t.Error("El hito", i, " no es del tipo correcto"	);
 		}
 	}
 }
-
-
-/*
-*	Comprueba el tipo de cada uno de los hitos.
-
-func TestUno(t *testing.T){
-	t.Log("Test Uno");
-
-		var prueba1 = Uno(0);
-		var prueba2 = Uno(1);
-		if reflect.TypeOf(prueba1).String() != "HitosIV.Hito"{
-			t.Error("El hito 1 no es del tipo correcto");
-		}
-
-		if reflect.TypeOf(prueba2).String() != "HitosIV.Hito"{
-			t.Error("El hito 2 no es del tipo correcto");
-		}
-
-	t.Log(reflect.TypeOf(prueba1));
-
-	if reflect.TypeOf(prueba1).String() == "Data"{
-		t.Log("Ha salido por aquí");
-	}else{
-		t.Log("Ha salido por allá");
-	}
-}
-*/
--- README.md
+++ README.md
@@ -3,6 +3,3 @@
 [![Build Status](https://travis-ci.org/JJ/HitosIV.svg?branch=master)](https://travis-ci.org/JJ/HitosIV)
 
 Paquete de Go para trabajar con hitos del proyecto de IV y CC
-
-
-Pruebas Alberto
